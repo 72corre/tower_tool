d8b2379695c7c5243c18cf6d440ebc06f26d741b
72corre <72corre@gmail.com>
2025-10-11 19:11:53 +0900

    Delete js directory

--- js/firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBS89pYuPITwJJh-Vtj6CPAFUZmm1XBqhQ",
-  authDomain: "tower-tool-average.firebaseapp.com",
-  projectId: "tower-tool-average",
-  storageBucket: "tower-tool-average.appspot.com",
-  messagingSenderId: "1023619102963",
-  appId: "1:1023619102963:web:c6d6c7a46460b796991f86",
-  measurementId: "G-M5ZK5FBVQQ"
-};
